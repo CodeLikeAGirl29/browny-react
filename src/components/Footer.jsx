0e31c6287458066c01fefed28393e8a39aefc0f2
codelikeagirl29 <codelikeagirl91@gmail.com>
2023-05-03 20:35:59 -0400

    added scroll to top btn

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import ScrollButton from './ScrollButton';
+import { Content } from './Styles';
 
 export default function Footer() {
   return (
     <div> <footer id="footer-copyright" className="footer-copyright">
       <div className="container">
         <div className="hm-footer-copyright text-center">
-          <p> &copy; copyright lindsey. <i className="fa-solid fa-code"></i> with <i className="fa-solid fa-heart"></i> by <a href="https://www.lindseyk.dev/">lindsey k productions</a>
+          <p> &copy; 2023 <i className="fa-solid fa-code"></i> with <i className="fa-solid fa-heart"></i> by <a href="https://www.lindseyk.dev/">lindsey</a>
           </p>
         </div>
       </div>
       <div id="scroll-Top">
-        <div className="return-to-top">
-          <i className="fa fa-angle-up " id="scroll-top"></i>
-        </div>
+      <Content />
+      <ScrollButton />
       </div>
     </footer></div>
   )
